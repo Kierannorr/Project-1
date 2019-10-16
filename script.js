dfa9d81f5a699e2fde5e79edea9408f1f86151f1
Kierannorr <56276036+Kierannorr@users.noreply.github.com>
2019-10-16 12:55:51 -0700

    Script js

---
+++ script.js
@@ -0,0 +1,15 @@
+function beta() {
+  window.location.replace("Aboutme.html");
+}
+function home() {
+  window.location.replace("index.html")
+}
+function delta() {
+  window.location.replace("Contactme.html")
+}
+function home() {
+  window.location.replace("index.html")
+}
+function gamma() {
+  window.location.replace("Resume.html")
+}
